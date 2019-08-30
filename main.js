8a3e267931e7c8fcf9e53c389a2b2a792bb03207
Daniel Spencer <37197986+SFallTech@users.noreply.github.com>
2019-08-30 22:37:57 +0300

    Fixing indents.

--- main.js
+++ main.js
@@ -9,74 +9,75 @@ const { join } = require("path");
  * @extends {Client}
  */
 class ReactionRole extends Client {
-    constructor () {
-        super();
+	constructor() {
+		super();
 
-        this.config = require("./config.js");
+		this.config = require("./config.js");
 
-        this.logger = require("./utils/Logger");
-        this.run()
-    }
+		this.logger = require("./utils/Logger");
+		this.run()
+	}
 
-    run() {
-        this.commands = new Collection();
-        this.aliases = new Collection();
-        this._commandsLoader();
-        this._eventsLoader();
-        this.login(this.config.bot.token);
-    }
+	run() {
+		this.commands = new Collection();
+		this.aliases = new Collection();
+		this._commandsLoader();
+		this._eventsLoader();
+		this.login(this.config.bot.token);
+	}
 
-    /**
+	/**
      * @description Laod all commands modules
      */
-    _commandsLoader() {
-        let commandsLength = 0;
-        const commands = readdirSync(join(__dirname, "/modules/Commands"));
-        commandsLength = commandsLength + commands.length;
-        commands.forEach((cmd) => {
-            try {
-                const command = new (require(join(__dirname, "/modules/Commands", cmd)))(this);
+	_commandsLoader() {
+		let commandsLength = 0;
+		const commands = readdirSync(join(__dirname, "/modules/Commands"));
+		commandsLength = commandsLength + commands.length;
+		commands.forEach((cmd) => {
+			try {
+				const command = new (require(join(__dirname, "/modules/Commands", cmd)))(this);
 
-                this.commands.set(command.help.name, command);
-                command.conf.aliases.forEach((alias) => {
-                    this.aliases.set(alias, command.help.name);
-                });
-            }
-            catch (error) {
-                return this.logger.error(`[Commands] Failed to load command ${cmd}: ${error}`);
-            }
-        });
-        return this.logger.log(`[Commands] - Loaded ${this.commands.size}/${commandsLength} commands.`);
-    }
+				this.commands.set(command.help.name, command);
+				command.conf.aliases.forEach((alias) => {
+					this.aliases.set(alias, command.help.name);
+				});
+			}
+			catch (error) {
+				return this.logger.error(`[Commands] Failed to load command ${cmd}: ${error}`);
+			}
+		});
+		return this.logger.log(`[Commands] - Loaded ${this.commands.size}/${commandsLength} commands.`);
+	}
 
-    /**
+	/**
      * @description Laod all events modules
      */
-    _eventsLoader() {
-        let eventLoadedLength = 0;
-        const events = readdirSync(join(__dirname, "/modules/Events"));
-        events.forEach((file) => {
-            try {
-                eventLoadedLength++;
-                const fileName = file.split('.')[0];
-                const event = new (require(join(__dirname, "/modules/Events", file)))(this);
-                this.on(fileName, (...args) => event.run(...args));
-                delete require.cache[require.resolve(join(__dirname, "/modules/Events", file))];
-            } catch (error) {
-                return this.logger.error(`[Events] - Failed to load event ${file}: ${error}`);
-            }
-        });
-        return this.logger.log(`[Events] - Loaded ${eventLoadedLength}/${events.length} events.`);
-    }
+	_eventsLoader() {
+		let eventLoadedLength = 0;
+		const events = readdirSync(join(__dirname, "/modules/Events"));
+		events.forEach((file) => {
+			try {
+				eventLoadedLength++;
+				const fileName = file.split(".")[0];
+				const event = new (require(join(__dirname, "/modules/Events", file)))(this);
+				this.on(fileName, (...args) => event.run(...args));
+				delete require.cache[require.resolve(join(__dirname, "/modules/Events", file))];
+			}
+			catch (error) {
+				return this.logger.error(`[Events] - Failed to load event ${file}: ${error}`);
+			}
+		});
+		return this.logger.log(`[Events] - Loaded ${eventLoadedLength}/${events.length} events.`);
+	}
 }
 
 process.on("uncaughtException", (err) => {
-    console.error("Uncaught Exception: ", err);
-    process.exit(1);
+	console.error("Uncaught Exception: ", err);
+	process.exit(1);
 });
 
 process.on("unhandledRejection", (err) => {
-    console.error("Uncaught Promise Error: ", err);
+	console.error("Uncaught Promise Error: ", err);
 });
 
 module.exports.client = new ReactionRole();
