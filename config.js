379604ceaa05624bba8f0595188faa0737838f5b
Daniel Spencer <37197986+SFallTech@users.noreply.github.com>
2019-08-30 22:17:08 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = config = {
+    "bot": {
+        "prefix": "=",
+        "token": "DISCORD_BOT_TOKEN",
+    },
+
+    "setup": {
+        "roles": ["Man", "Woman"],
+        "emojis": ["👨", "👩"],
+    }
+};
---
+++ utils/Logger.js
@@ -0,0 +1,36 @@
+const chalk = require("chalk");
+const moment = require("moment");
+
+class Logger {
+    /**
+     * Used to display some messages.
+     * @function options.bot.logger.log
+     * @param {string} message Message(s) to be shown in the log.
+     * @returns {void}
+     */
+    static log(message) {
+        return console.log(`[${chalk.keyword("green")(`${moment().format("HH:mm:ss")}`)}] - [${chalk.keyword("green")("LOG")}] ${message}`);
+    }
+
+    /**
+     * Used to display warnings messages.
+     * @function options.bot.logger.warn
+     * @param {string} message Message(s) to be shown in the warn log.
+     * @returns {void}
+     */
+    static warn(message) {
+        return console.log(`[${chalk.keyword("orange")(`${moment().format("DD/MM HH:mm:ss")}`)}] - ${chalk.keyword("orange")("WARNING")}] ${message}`);
+    }
+
+    /**
+     * Used to display errors messages.
+     * @function options.bot.logger.error
+     * @param {string} message Message(s) to be shown in the error log.
+     * @returns {void}
+     */
+    static error(message) {
+        return console.log(`[${chalk.keyword("red")(`${moment().format("DD/MM HH:mm:ss")}`)}] - ${chalk.keyword("red")("ERROR")}] ${message}`);
+    }
+}
+
+module.exports = Logger;
